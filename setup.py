64948cf6e4d66e5406f1fc0039e67bd53f260f65
SDeyGitHubber <114286007+SDeyGitHubber@users.noreply.github.com>
2023-08-18 01:19:32 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of Wafers project using MLOps workflow',
+    author='Shouvik Dey',
+    license='MIT',
+)
